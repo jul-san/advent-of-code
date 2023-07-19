e8424155a2e7e37ca9f9cc3846dd31202da8f97b
Julian Sanchez <julianwsan@gmail.com>
2023-07-18 21:18:24 -0400

    😼 day 1 done

--- 2015/01/1.py
+++ 2015/01/1.py
@@ -2,12 +2,17 @@ with open("./input.txt", 'r') as file:
     contents = file.read();
     
     counter = 0
+    position = 0
 
-    for x in contents:
+    for i, x in enumerate(contents):
         if x == "(":
             counter += 1
         elif x == ")":
             counter -= 1
+        if counter == -1:
+            position = i + 1
+            break
 
-    print(counter)
+file.close()
+print(position)
         
